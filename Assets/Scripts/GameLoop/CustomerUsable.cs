b728f24d8aec9306e065a748ea8c70bc85f50cef
Albin Fyhr <albinfyhr@gmail.com>
2023-04-02 16:28:46 +0200

    Update CustomerUsable.cs

--- Assets/Scripts/GameLoop/CustomerUsable.cs
+++ Assets/Scripts/GameLoop/CustomerUsable.cs
@@ -5,14 +5,20 @@ using FishNet.Object;
 
 public class CustomerUsable : NetworkBehaviour, IUsable
 {
+    ////////////////////////////////////////////////////////////////
+
     public CustomerSpawn customerSpawn { get; set; } = null;
     public bool hasTakenOrder { get; set; } = false;
 
     public bool hold { get; set; } = false;
+
+    ////////////////////////////////////////////////////////////////
 
     public void Use()
     {
         UIManager.Instance.TakeOrder( customerSpawn.order );
         hasTakenOrder = true;
     }
+
+    ////////////////////////////////////////////////////////////////
 }
