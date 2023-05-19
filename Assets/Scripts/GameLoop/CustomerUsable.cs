0d4c69ad2455867de7c264aedda89b6b151807dd
Albin Fyhr <albinfyhr@gmail.com>
2023-05-19 12:30:52 +0200

    Update customerusable with serverrpc

--- Assets/Scripts/GameLoop/CustomerUsable.cs
+++ Assets/Scripts/GameLoop/CustomerUsable.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
+using FishNet.Object.Synchronizing;
 
 public class CustomerUsable : NetworkBehaviour, IUsable
 {
     ////////////////////////////////////////////////////////////////
 
     public CustomerSpawn customerSpawn { get; set; } = null;
-    public bool hasTakenOrder { get; set; } = false;
+
+    [SyncVar]
+    public bool hasTakenOrder = false;
 
     public bool hold { get; set; } = false;
 
@@ -19,8 +22,13 @@ public class CustomerUsable : NetworkBehaviour, IUsable
         if( hasTakenOrder == true )
             return;
 
-        customerSpawn.order.orderStatus = Order.ORDER_STATUS.TAKEN;
         UIManager.Instance.TakeOrder( customerSpawn.order );
+        SetOrderTakenServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SetOrderTakenServerRpc()
+    {
         hasTakenOrder = true;
     }
 
